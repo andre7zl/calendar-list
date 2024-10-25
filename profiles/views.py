b930b2113f7a668a050567970ccd08d73d5e91d3
Andre <andreivo637@gmail.com>
2024-10-25 17:35:14 -0300

    adicionando o usuario automaticamente ao grupo Discente quando registrar

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,21 @@
 from django.views.generic.edit import CreateView
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.urls import reverse_lazy
 from .forms import UsuarioForm
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
 class CreateUser(CreateView):
-    template_name = "register/register.html"  # Ajuste o caminho se necessário
+    template_name = "register/register.html"
     form_class = UsuarioForm
-    success_url = reverse_lazy('login')  # Substitua pelo nome correto da URL
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        grupo = get_object_or_404(Group, name="Discente")
+        url = super().form_valid(form)
+        self.object.groups.add(grupo)
+        self.object.save()
+        return url
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)

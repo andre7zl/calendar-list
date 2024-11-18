bdf4e63f3abc26e7ae8f654e69f380ad7900a193
Andre <andreivo637@gmail.com>
2024-11-18 09:26:51 -0300

    adicionando cards de dados do usuario

--- profiles/models.py
+++ profiles/models.py
@@ -6,3 +6,21 @@ class CustomUser(AbstractUser):
     id = models.BigAutoField(primary_key=True)
 
     turma = models.ForeignKey('classes.Turma', on_delete=models.SET_NULL, blank=True, null=True)
+
+    @property
+    def cargo(self):
+        if self.groups.filter(name="Tutor").exists():
+            return "Aluno e Tutor"
+        elif self.groups.filter(name="Discente").exists():
+            return "Aluno"
+        elif self.groups.filter(name="Docente").exists():
+            return "Professor"
+        return "Indefinido"
+    
+    @property
+    def perfilturma(self):
+        if self.groups.filter(name="Discente").exists():
+            return self.turma 
+        elif self.groups.filter(name="Docente").exists():
+            return "Voce é professor :D"
+        return "Indefinido"

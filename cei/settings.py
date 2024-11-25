5e2ee2b53351c85791a4d47bb70211f2076c1fbd
Andre <andreivo637@gmail.com>
2024-11-25 18:07:57 -0300

    negando o acesso da url da api

--- cei/settings.py
+++ cei/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'tasks.middleware.BlockDirectAccessMiddleware',
 ]
 
 ROOT_URLCONF = 'cei.urls'

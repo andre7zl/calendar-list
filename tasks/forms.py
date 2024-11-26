559d3c1a75e7d3e9a327d377b69001d2f9c50ef5
Andre <andreivo637@gmail.com>
2024-11-26 13:54:00 -0300

    estilizando form de cadastrar evento

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,10 +2,8 @@ from django import forms
 from .models import Task
 from django.forms import DateInput
 from django import forms
-from classes import models
 
 class TaskForm(forms.ModelForm):
-    turma = forms.ModelChoiceField(queryset=models.Turma.objects.all(), required=True)
     class Meta:
         model = Task
         fields = ["title", "description", "turma", "start_date", "end_date", "start_time", "end_time"]
@@ -15,38 +13,23 @@ class TaskForm(forms.ModelForm):
                 attrs={"class": "form-control", "placeholder": "Insira o título do evento"}
             ),
             "description": forms.Textarea(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Insira a descrição do evento",
-                }
+                attrs={"class": "form-control", "placeholder": "Insira a descrição do evento"}
             ),
             "turma": forms.Select(
-                attrs={
-                    "class": "form-select",  # Adicionando a classe do Bootstrap
-                    "aria-label": "Selecionar turma"  # Para acessibilidade
-                }
+                attrs={"class": "form-select", "aria-label": "Selecionar turma"}
             ),
-            "start_date": DateInput(
-                attrs={"class": "form-control"},
-            ),
-            "end_date": DateInput(
-                attrs={"class": "form-control"},
-            ),
-            "start_time": DateInput(
-                attrs={"class": "form-control"},
-            ),
-            "end_time": DateInput(
-                attrs={"class": "form-control"},
-            ),
+            "start_date": DateInput(attrs={"class": "form-control", "placeholder": "dd/mm/aaaa"}),
+            "end_date": DateInput(attrs={"class": "form-control", "placeholder": "dd/mm/aaaa"}),
+            "start_time": DateInput(attrs={"class": "form-control", "placeholder": "hh:mm:ss"}),
+            "end_time": DateInput(attrs={"class": "form-control", "placeholder": "hh:mm:ss"}),
         }
-        exclude = ["user"]
 
     def __init__(self, *args, **kwargs):
         super(TaskForm, self).__init__(*args, **kwargs)
 
     def save(self, commit=True):
         task = super().save(commit=False)
-        task.id_turma = self.cleaned_data['turma'].id  # Armazena o ID da turma selecionada
+        task.id_turma = self.cleaned_data['turma'].id
         if commit:
             task.save()
         return task

81a9395c16eb8cad9033845f4c11f87f62fd1ec8
Andre <andreivo637@gmail.com>
2024-11-26 13:34:38 -0300

    estilizando form de cadastro de turma

--- classes/forms.py
+++ classes/forms.py
@@ -5,3 +5,12 @@ class TurmaForm(forms.ModelForm):
     class Meta:
         model = Turma
         fields = ['nome', 'serie', 'turno', 'curso']
+        widgets = {
+            'nome': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Coloque em sigla. Ex: INFO4V'
+            }),
+            'serie': forms.Select(attrs={'class': 'form-select'}),
+            'turno': forms.Select(attrs={'class': 'form-select'}),
+            'curso': forms.Select(attrs={'class': 'form-select'}),
+        }

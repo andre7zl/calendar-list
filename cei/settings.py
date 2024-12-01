783c546ac5743a8ed9f1d5ff8bef96e91df76472
Andre <andreivo637@gmail.com>
2024-12-01 12:18:38 -0300

    trocando links cdn por arquivos estaticos

--- cei/settings.py
+++ cei/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 AUTH_USER_MODEL = 'profiles.CustomUser'

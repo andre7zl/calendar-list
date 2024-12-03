676cf464133ffc3b151f297fdcb172c54a6fb572
Andre <andreivo637@gmail.com>
2024-12-02 22:09:25 -0300

    adicionando eventos na API

--- tasks/models.py
+++ tasks/models.py
@@ -5,8 +5,8 @@ from profiles.models import CustomUser
 class Task(models.Model):
     id = models.BigAutoField(primary_key=True)
 
-    title = models.CharField(max_length=20)
-    description = models.TextField(max_length=150)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
     start_date = models.DateField()
     end_date = models.DateField()
     start_time = models.TimeField()
